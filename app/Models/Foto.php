42701e29c8e7c4e0c6cd806c4760232a6f6ae739
Your Name <you@example.com>
2022-10-29 22:24:59 -0500

    Lab10

---
+++ app/Models/Foto.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Foto extends Model
+{
+    use HasFactory;
+    public function User(){
+        return $this->belognsTo(User::class);
+    }
+    public function Comentario(){
+        return $this->hasMany(Comentario::class);
+    }
+}
+
